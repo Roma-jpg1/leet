f30d4e559079e7900c11a1be366e749e3cd046db
Roma-jpg1 <rpetrov2006@mail.ru>
2026-04-02 12:22:27 +0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (96.77%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -0,0 +1,23 @@
+import "sort"
+func intersection(nums1 []int, nums2 []int) []int {
+    sort.Ints(nums1)
+    sort.Ints(nums2)
+
+    i, j := 0, 0
+    result := []int{}
+    for i<len(nums1) && j<len(nums2){
+        if nums1[i] == nums2[j]{
+            if len(result) == 0 || result[len(result)-1] != nums1[i] {
+                result = append(result, nums1[i])
+            }
+            i++
+            j++
+        } else if nums1[i] > nums2[j] {
+            j++
+        } else{
+            i++
+        }
+    }
+    return result
+    
+}
